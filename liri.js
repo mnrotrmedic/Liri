47e0c44d13a8272752282641cc3c6c6e1e965e7b
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-02-17 11:25:48 -0600

    packages-1

---
+++ liri.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const dotenv = require("dotenv");
+const moment = require("moment");
+const spotify = require("node-spotify-api");
+const inquirer = require('inquirer');
+
+
+/* var spotify = new Spotify({
+    id: 68062146c5fe4c42a720a225817a807b,
+    secret: 35dce250905d40cd9802eeafc90b4ce6
+  }); */
+
