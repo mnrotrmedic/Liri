738a07d06c96d4d9eb831326af2d37bae8ec823d
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-02-17 12:27:29 -0600

    more setup

--- liri.js
+++ liri.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
+const keys = require("./keys.js");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const moment = require("moment");
 const spotify = require("node-spotify-api");
 const inquirer = require('inquirer');
+const fs = require("fs");
 
 
-/* var spotify = new Spotify({
-    id: 68062146c5fe4c42a720a225817a807b,
-    secret: 35dce250905d40cd9802eeafc90b4ce6
-  }); */
+spotify = new Spotify(keys.spotify);
+
+
 
